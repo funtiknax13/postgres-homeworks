10582d32d9bc8254f43e20a7a38396d289ea09a8
funtiknax13 <sergeyfedorov13@yandex.ru>
2023-06-10 11:58:18 +0300

    Create tables with SQL Add data with psycopg2

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customer_id varchar(5) PRIMARY KEY,
+	company_name varchar(150) NOT NULL,
+	contact_name varchar(100)
+);
+
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50),
+	title varchar(100),
+	birth_date varchar(10),
+	notes text
+);
+
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(5) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date varchar(10),
+	ship_city varchar(100)
+
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,38 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+from keys import USERNAME, USER_PASSWORD
+
+
+with psycopg2.connect(
+    host="localhost",
+    database="north",
+    user=USERNAME,
+    password=USER_PASSWORD
+) as conn:
+    with conn.cursor() as cur:
+        with open("north_data/customers_data.csv", "r", encoding="utf-8") as file:
+            data = csv.DictReader(file)
+            for row in data:
+                cur.execute("INSERT INTO customers VALUES (%s, %s, %s)",
+                            (row["customer_id"], row["company_name"], row["contact_name"]))
+
+        with open("north_data/employees_data.csv", "r", encoding="utf-8") as file:
+            data = csv.DictReader(file)
+            for row in data:
+                cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+                            (row["employee_id"], row["first_name"], row["last_name"],
+                             row["title"], row["birth_date"], row["notes"]))
+
+        with open("north_data/orders_data.csv", "r", encoding="utf-8") as file:
+            data = csv.DictReader(file)
+            for row in data:
+                cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+                            (row["order_id"], row["customer_id"], row["employee_id"],
+                             row["order_date"], row["ship_city"]))
+
+conn.close()
+
+
+
